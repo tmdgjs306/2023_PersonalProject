81f886e159ca1a11c365cd8c0f3bdb1994de49cf
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2023-07-20 13:34:23 +0900

    FactoryDataSend_v0.2
    Json 형식으로 값 전달하는 부분 추가

--- FactoryData/Day04_wifi02/Day04_wifi02.ino
+++ FactoryData/Day04_wifi02/Day04_wifi02.ino
@@ -1,30 +1,45 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
-#include "oled_u8g2.h"
-#include <stdio.h>
-#include <string.h>
+#include <time.h> 
+#include <ArduinoJson.h>
 
-//WIFI Name
-const char* ssid = "SmartFactory"; 
+//WIFI setting
+const char* ssid = "KEB_INHA"; 
+const char* password = "inha123*"; 
 
-//WIFI Pwd
-const char* password = "inha4885"; 
-
-WebServer server(80); // Create WebServer Object, port 
-
+//sensor setting
 int sensor = A2;    
 int Vo;
 float R1 = 10000;
 float logR2, R2, T, Tc, Tf;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
-//OLED
-OLED_U8G2 oled;
+//count setting
+int count = 0;
 
+//Time setting
+int timezone = 3; 
+int dst = 0; 
+unsigned long previousMillis = 0;
+const long interval = 1000; 
 
+//JSON
+char buffer[96];
+
+//WebServer Setting
+WebServer server(80); // Create WebServer Object, port 
+void printConnectMsg(){
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: "); 
+  Serial.println(WiFi.localIP()); //print assigned ip address 
+
+}
+ //온도 센서의 값을 읽고 온도로 변환
 float getTemp(){
-  Vo = analogRead(sensor);                               // 센서의 값을 읽고 온도로 변환
+  Vo = analogRead(sensor);
   R2 = R1 * (4095.0 / (float)Vo - 1.0);
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
@@ -32,77 +47,98 @@ float getTemp(){
   return Tc;
 }
 
-void handleRootEvent() { // root(/)
-  String clientIp = server.client().remoteIP().toString();
+//온도 센서의 값을 화씨 온도로 변환 
+float tcToTf(float Tc){
+  return (Tc * 9.0/5.0) +32.0;
+}
 
-  Tc = getTemp();
-  Tf =(Tc * 9.0/5.0) +32.0;
+// 현재 카운트 값 반환
+int getCount(){
+  return count;
+} 
+
+// IP 주소 XXX 처리 
+String ipAddressConverter(String IpAddress){
   int a1,a2,a3,a4;
-  String str = server.client().remoteIP().toString();
-  sscanf(str.c_str(),"%d.%d.%d.%d", &a1, &a2,&a3,&a4);
-  //char *token = strtok(str,"."); // 첫 번째 token을 반환한다
-  //int count =0;
-  String privateIP ="";
-  privateIP = String(a1)+".XXX.XXX."+String(a2);
-  // while (token != NULL) // delimiter 중 하나가 str[]에 존재할 경우 계속해서 token을 출력한다.
-  // {
-  //   if(count==1 || count ==2){
-  //     privateIP+=*+".";
-  //   }
-  //   else{
-  //     privateIP+=token+"."
-  //   }
-  //   token = strtok(NULL, ".");
-  //   count++;
-  // }
-  String message = "Welcome Inha SmartFactory WebServer!\n\n";
-  message += "Temperature: "+String(Tc)+"C "+"( "+String(Tf) + "F)\n";
-  message += "Your IP: "+privateIP+"\n";
+  sscanf(IpAddress.c_str(),"%d.%d.%d.%d", &a1, &a2, &a3, &a4);
+  return String(a1)+".XXX.XXX."+String(a4);
+} 
 
+
+/// Web 핸들러 
+// 루트로 접속했을때 핸들러 
+void handleRootEvent() {
+  String clientIp = server.client().remoteIP().toString();
+  String privateIP = ipAddressConverter(clientIp);
+  String message = "Hello World!!";
+  
   Serial.print("Main Page from :"); // Page Enter 
   Serial.println(clientIp);
-  server.send(200 , "text/plain", message);
-  Serial.println(message); 
-  Serial.print(Tc);
-  Serial.print("C (");
-  Serial.print(Tf);
-  Serial.print("F)");            
+
+  server.send(200 , "text/plain", message);   
+}
+// getTemp 로 접속했을때 핸들러 
+void getTempJson(){
+  float temp = getTemp();
+  StaticJsonDocument<200> doc;
+  JsonObject root = doc.to<JsonObject>();
+  root["Sensor"] = "temperature";
+  root["Value"] = temp;
+
+  serializeJson(root,buffer);
+  server.send(200 , "text/Json",buffer); 
+}
+
+void getCountJson(){
+  int count = getCount();
+  StaticJsonDocument<200> doc;
+  JsonObject root = doc.to<JsonObject>();
+  root["Sensor"] = "ultrasonicWave";
+  root["Value"] = count;
+
+  serializeJson(root,buffer);
+  server.send(200 , "text/Json",buffer); 
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200); // ESP32 baud rate 
+
+  Serial.begin(115200); // ESP32 baud rate
+
+  //WIFI Setting  
   WiFi.mode(WIFI_STA); 
   WiFi.begin(ssid, password); 
   Serial.println("");
-  
-  //  wait for connection
+
   while(WiFi.status() != WL_CONNECTED) {
   delay(500);
   Serial.print(".");
   }
+  printConnectMsg(); //시리얼에 IP 주소 정보 출력 
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: "); 
-  Serial.println(WiFi.localIP()); //print assigned ip address 
-
-  // root(/) Enter Function 
-  server.on("/", handleRootEvent); //event handlong function
+  server.on("/", handleRootEvent); // root(/) Enter Function 
+  server.on("/getTemp",getTempJson); // (/getTemp) 접속시 Json 포맷으로 온도 데이터 전송 
+  server.on("/getCount",getCountJson); // (/getCount) 접속시 Json 포맷으로 카운트 값 전송 
   server.begin(); // Server Start 
   Serial.println("HTTP server started");
 
-  //OLED Setup
-  oled.setup();        
+  //time setting
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov"); 
+  Serial.println("\nWaiting for time"); 
+  while (!time(nullptr)) { 
+    Serial.print("."); 
+    delay(1000); 
+  }
+  Serial.println("");         
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient(); // Client session Receive
-  oled.setLine(1, "*Welcom KGU");               // OLED 첫 번째 줄 : 시스템 이름
-  oled.setLine(2, "smart Factory");              // OLED 두 번째 줄 : count 값
-  oled.setLine(3, "---------------------");   // OLED 세 번째 줄 
-  oled.display();
-  delay(500);
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+
+    time_t now = time(nullptr);
+    Serial.println(ctime(&now));
+  }
 }
